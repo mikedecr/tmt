ec1f25defa0922d01462af8c1ffe8b6c755ba6cc
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-01-11 00:49:52 -0600

    more abstract solution

--- hask/train/05.hs
+++ hask/train/05.hs
@@ -1,21 +1,19 @@
-import Data.List (intercalate, concat)
-
+-- build a Caesar cypher for shift size n
 caesar :: Int -> [(Char, Char)]
 caesar n = do
     let alphabet = ['a'..'z']
     let shifted = drop n alphabet ++ take n alphabet
-    let c = zip shifted alphabet
-    let specials = [(',', ','), (' ', ' ')]
-    c ++ specials 
+    -- specials map to themselves
+    let specials = [',', ' ']
+    -- return cypher as [(k, v)]
+    zip shifted alphabet ++ zip specials specials
+
+-- let's simplify and say a hashap is a list [(k, v), ...]
+-- then get lst k = v
+-- might want to constrain keys to be unique...
+get :: [(a, a)] -> a -> a
+get lst key = [snd item | item <- lst, fst item == key]
 
 input = "zirm, zmhm, zmgm"
-
--- this is hacky shit
--- could separate the abstract tuple lookup from the cypher
--- or just learn how haskell does hash maps
-caesarGet :: Int -> Char -> Char
-caesarGet n c = [snd x | x <- caesar n, fst x == c]
-
-ansTrain05 = concatMap (caesarGet 4) input 
-
+ansTrain05 = concatMap (get (caesar 4)) input
 
