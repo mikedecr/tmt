80e373fd1c381c7b9f532445872be514a0076992
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-01-11 21:31:39 -0600

    1 has no fun programming

---
+++ R/01.R
@@ -0,0 +1,10 @@
+# iliad
+# adventures of huckleberry finn
+# moby dick
+# hamlet
+# emma
+# restaurant at the end of the universe
+hints = c('i', 'a', 'm', 'h', 'e', 'r', 'e')
+
+(ans_01 = "The Master Theorem")
+
